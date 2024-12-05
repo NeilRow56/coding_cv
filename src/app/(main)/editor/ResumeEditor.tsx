507d08c270aa750ea15f980977ae36ee4025dd77
NeilRow56 <neilrowland56@gmail.com>
2024-12-05 10:14:07 +0000

    Added Personal Info form 1st stage

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -4,12 +4,13 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import React from 'react'
 import GeneralInfoForm from './forms/GeneralInfoForm'
+import PersonalInfoForm from './forms/PersonalInfoForm'
 
 export default function ResumeEditor() {
   return (
     <div className='flex grow flex-col'>
       <header className='space-y-1.5 border-b px-3 py-5 text-center'>
-        <h1 className='text-2xl font-bold'>Design your resume</h1>
+        <h1 className='text-2xl font-bold text-blue-600'>Design your resume</h1>
         <p className='text-sm text-muted-foreground'>
           Follow the steps below to create your resume. Your progress will be
           saved automatically.
@@ -17,8 +18,8 @@ export default function ResumeEditor() {
       </header>
       <main className='relative grow'>
         <div className='absolute bottom-0 top-0 flex w-full'>
-          <div className='w-full p-3 md:w-1/2'>
-            <GeneralInfoForm />
+          <div className='w-full overflow-y-auto p-3 md:w-1/2'>
+            <PersonalInfoForm />
           </div>
           <div className='grow md:border-r' />
           <div className='hidden w-1/2 bg-neutral-50 md:flex'>Right</div>
