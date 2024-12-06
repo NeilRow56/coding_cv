a5f99eca3d50c18973fde9683e624e667c2505ad
NeilRow56 <neilrowland56@gmail.com>
2024-12-06 11:59:09 +0000

    ResumePreview.tsx updated for personal Information header

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,7 +1,8 @@
 import useDimensions from '@/hooks/useDimensions'
 import { cn } from '@/lib/utils'
 import { ResumeValues } from '@/lib/validation'
-import React, { useRef } from 'react'
+import Image from 'next/image'
+import React, { useEffect, useRef, useState } from 'react'
 
 interface ResumePreviewProps {
   resumeData: ResumeValues
@@ -31,9 +32,85 @@ export default function ResumePreview({
           zoom: (1 / 794) * width
         }}
       >
-        <h1 className='p-6 text-3xl font-bold'>
-          This text should change with the size of the container div
-        </h1>
+        <pre>{JSON.stringify(resumeData, null, 2)}</pre>
+        <PersonalInfoHeader resumeData={resumeData} />
+      </div>
+    </div>
+  )
+}
+
+interface ResumeSectionProps {
+  resumeData: ResumeValues
+}
+
+function PersonalInfoHeader({ resumeData }: ResumeSectionProps) {
+  const {
+    photo,
+    firstName,
+    lastName,
+    jobTitle,
+    city,
+    country,
+    phone,
+    email
+    //   colorHex,
+    //   borderStyle,
+  } = resumeData
+
+  const [photoSrc, setPhotoSrc] = useState(photo instanceof File ? '' : photo)
+
+  useEffect(() => {
+    const objectUrl = photo instanceof File ? URL.createObjectURL(photo) : ''
+    if (objectUrl) setPhotoSrc(objectUrl)
+    if (photo === null) setPhotoSrc('')
+    return () => URL.revokeObjectURL(objectUrl)
+  }, [photo])
+
+  return (
+    <div className='flex items-center gap-6'>
+      {photoSrc && (
+        <Image
+          src={photoSrc}
+          width={100}
+          height={100}
+          alt='Author photo'
+          className='aspect-square object-cover'
+          // style={{
+          //   borderRadius:
+          //     borderStyle === BorderStyles.SQUARE
+          //       ? "0px"
+          //       : borderStyle === BorderStyles.CIRCLE
+          //         ? "9999px"
+          //         : "10%",
+          // }}
+        />
+      )}
+      <div className='space-y-2.5'>
+        <div className='space-y-1'>
+          <p
+            className='text-3xl font-bold'
+            //   style={{
+            //     color: colorHex,
+            //   }}
+          >
+            {firstName} {lastName}
+          </p>
+          <p
+            className='font-medium'
+            //   style={{
+            //     color: colorHex,
+            //   }}
+          >
+            {jobTitle}
+          </p>
+        </div>
+        <p className='text-xs text-gray-500'>
+          {city}
+          {city && country ? ', ' : ''}
+          {country}
+          {(city || country) && (phone || email) ? ' • ' : ''}
+          {[phone, email].filter(Boolean).join(' • ')}
+        </p>
       </div>
     </div>
   )
