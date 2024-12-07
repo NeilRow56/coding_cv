10ac7d7254d1d2f1677975b9155b8aa2ee190611
NeilRow56 <neilrowland56@gmail.com>
2024-12-07 14:41:59 +0000

    Added drag and drop functionality to Work Experience and Education forms

--- src/app/(main)/editor/forms/EducationForm.tsx
+++ src/app/(main)/editor/forms/EducationForm.tsx
@@ -2,7 +2,6 @@ import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -10,10 +9,29 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { EditorFormProps } from '@/lib/types'
+import { cn } from '@/lib/utils'
 import { educationSchema, EducationValues } from '@/lib/validation'
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { restrictToVerticalAxis } from '@dnd-kit/modifiers'
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { GripHorizontal } from 'lucide-react'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useFieldArray, useForm, UseFormReturn } from 'react-hook-form'
 
 export default function EducationForm({
@@ -33,8 +51,7 @@ export default function EducationForm({
       if (!isValid) return
       setResumeData({
         ...resumeData,
-        educations:
-          values.educations?.filter(education => education !== undefined) || []
+        educations: values.educations?.filter(edu => edu !== undefined) || []
       })
     })
     return unsubscribe
@@ -45,25 +62,55 @@ export default function EducationForm({
     name: 'educations'
   })
 
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
+  )
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event
+
+    if (over && active.id !== over.id) {
+      const oldIndex = fields.findIndex(field => field.id === active.id)
+      const newIndex = fields.findIndex(field => field.id === over.id)
+      move(oldIndex, newIndex)
+      return arrayMove(fields, oldIndex, newIndex)
+    }
+  }
+
   return (
     <div className='mx-auto max-w-xl space-y-6'>
       <div className='space-y-1.5 text-center'>
         <h2 className='text-2xl font-semibold'>Education</h2>
         <p className='text-sm text-muted-foreground'>
-          Add as many education items as you like.
+          Add as many educations as you like.
         </p>
       </div>
       <Form {...form}>
         <form className='space-y-3'>
-          {fields.map((field, index) => (
-            <EducationItem
-              id={field.id}
-              key={field.id}
-              index={index}
-              form={form}
-              remove={remove}
-            />
-          ))}
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+            modifiers={[restrictToVerticalAxis]}
+          >
+            <SortableContext
+              items={fields}
+              strategy={verticalListSortingStrategy}
+            >
+              {fields.map((field, index) => (
+                <EducationItem
+                  id={field.id}
+                  key={field.id}
+                  index={index}
+                  form={form}
+                  remove={remove}
+                />
+              ))}
+            </SortableContext>
+          </DndContext>
           <div className='flex justify-center'>
             <Button
               type='button'
@@ -77,7 +124,7 @@ export default function EducationForm({
                 })
               }
             >
-              Add education information
+              Add education
             </Button>
           </div>
         </form>
@@ -94,11 +141,34 @@ interface EducationItemProps {
 }
 
 function EducationItem({ id, form, index, remove }: EducationItemProps) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging
+  } = useSortable({ id })
+
   return (
-    <div className='space-y-3 rounded-md border bg-background p-3'>
+    <div
+      className={cn(
+        'space-y-3 rounded-md border bg-background p-3',
+        isDragging && 'relative z-50 cursor-grab shadow-xl'
+      )}
+      ref={setNodeRef}
+      style={{
+        transform: CSS.Transform.toString(transform),
+        transition
+      }}
+    >
       <div className='flex justify-between gap-2'>
         <span className='font-semibold'>Education {index + 1}</span>
-        <GripHorizontal className='size-5 cursor-grab text-muted-foreground' />
+        <GripHorizontal
+          className='size-5 cursor-grab text-muted-foreground focus:outline-none'
+          {...attributes}
+          {...listeners}
+        />
       </div>
       <FormField
         control={form.control}
