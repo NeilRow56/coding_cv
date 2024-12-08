edbbfe414d6c448ea3c19a2669aa75b42b2697d2
NeilRow56 <neilrowland56@gmail.com>
2024-12-08 05:11:49 +0000

    Completed server actions and updates to auto save to database and Blob storage

--- src/app/(main)/editor/useAutoSaveResume.tsx
+++ src/app/(main)/editor/useAutoSaveResume.tsx
@@ -1,10 +1,21 @@
+import { useToast } from '@/hooks/use-toast'
 import useDebounce from '@/hooks/useDebounce'
 import { ResumeValues } from '@/lib/validation'
+import { useSearchParams } from 'next/navigation'
 
 import { useEffect, useState } from 'react'
+import { saveResume } from './actions'
+import { fileReplacer } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 
 export default function useAutoSaveResume(resumeData: ResumeValues) {
+  const searchParams = useSearchParams()
+
+  const { toast } = useToast()
+
   const debouncedResumeData = useDebounce(resumeData, 1500)
+
+  const [resumeId, setResumeId] = useState(resumeData.id)
 
   const [lastSavedData, setLastSavedData] = useState(
     structuredClone(resumeData)
@@ -12,22 +23,87 @@ export default function useAutoSaveResume(resumeData: ResumeValues) {
 
   const [isSaving, setIsSaving] = useState(false)
 
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    setIsError(false)
+  }, [debouncedResumeData])
+
   useEffect(() => {
     async function save() {
-      setIsSaving(true)
+      try {
+        setIsSaving(true)
+        setIsError(false)
 
-      await new Promise(resolve => setTimeout(resolve, 1500))
-      setLastSavedData(structuredClone(debouncedResumeData))
-      setIsSaving(false)
+        const newData = structuredClone(debouncedResumeData)
+
+        const updatedResume = await saveResume({
+          ...newData,
+          ...(JSON.stringify(lastSavedData.photo, fileReplacer) ===
+            JSON.stringify(newData.photo, fileReplacer) && {
+            photo: undefined
+          }),
+          id: resumeId
+        })
+
+        setResumeId(updatedResume.id)
+        setLastSavedData(newData)
+
+        if (searchParams.get('resumeId') !== updatedResume.id) {
+          const newSearchParams = new URLSearchParams(searchParams)
+          newSearchParams.set('resumeId', updatedResume.id)
+          window.history.replaceState(
+            null,
+            '',
+            `?${newSearchParams.toString()}`
+          )
+        }
+      } catch (error) {
+        setIsError(true)
+        console.error(error)
+        const { dismiss } = toast({
+          variant: 'destructive',
+          description: (
+            <div className='space-y-3'>
+              <p>Could not save changes.</p>
+              <Button
+                variant='secondary'
+                onClick={() => {
+                  dismiss()
+                  save()
+                }}
+              >
+                Retry
+              </Button>
+            </div>
+          )
+        })
+      } finally {
+        setIsSaving(false)
+      }
     }
+
+    console.log(
+      'debouncedResumeData',
+      JSON.stringify(debouncedResumeData, fileReplacer)
+    )
+    console.log('lastSavedData', JSON.stringify(lastSavedData, fileReplacer))
 
     const hasUnsavedChanges =
       JSON.stringify(debouncedResumeData) !== JSON.stringify(lastSavedData)
 
-    if (hasUnsavedChanges && debouncedResumeData && !isSaving) {
+    if (hasUnsavedChanges && debouncedResumeData && !isSaving && !isError) {
       save()
     }
-  }, [debouncedResumeData, isSaving, lastSavedData])
+  }, [
+    debouncedResumeData,
+    isSaving,
+    lastSavedData,
+    isError,
+    resumeId,
+    searchParams,
+    toast
+  ])
 
   return {
     isSaving,
