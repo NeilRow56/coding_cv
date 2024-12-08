132358a2711c338c129ae2c97b61bae7a67e06d0
NeilRow56 <neilrowland56@gmail.com>
2024-12-08 10:17:33 +0000

    Fetch and display Resume summary details on resumes page- with link to editor form

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,22 +1,52 @@
-import { Button } from '@/components/ui/button'
-import { PlusSquare } from 'lucide-react'
+import db from '@/lib/db'
+
+import { resumeDataInclude } from '@/lib/types'
+import { auth } from '@clerk/nextjs/server'
 import { Metadata } from 'next'
-import Link from 'next/link'
-import React from 'react'
+import ResumeItem from './ResumeItem'
 
 export const metadata: Metadata = {
   title: 'Your resumes'
 }
 
-export default function ResumePage() {
+export default async function ResumesPage() {
+  const { userId } = await auth()
+
+  if (!userId) {
+    return null
+  }
+  // We use the Promise.all so that we can get the count and resumes at the same time.
+
+  const [resumes, totalCount] = await Promise.all([
+    db.resume.findMany({
+      where: {
+        userId: userId
+      },
+      orderBy: {
+        updatedAt: 'desc'
+      },
+      include: resumeDataInclude
+    }),
+    db.resume.count({
+      where: {
+        userId: userId
+      }
+    })
+  ])
+
+  //TODO: check quota for non premium users
+
   return (
     <main className='mx-auto w-full max-w-7xl space-y-6 px-3 py-6'>
-      <Button asChild className='mx-auto flex w-fit gap-2'>
-        <Link href='/editor'>
-          <PlusSquare className='size=5' />
-          New resume
-        </Link>
-      </Button>
+      <div className='space-y-1'>
+        <h1 className='text-3xl font-bold'>Your resumes</h1>
+        <p>Total: {totalCount}</p>
+      </div>
+      <div className='flex w-full grid-cols-2 flex-col gap-3 sm:grid md:grid-cols-3 lg:grid-cols-4'>
+        {resumes.map(resume => (
+          <ResumeItem key={resume.id} resume={resume} />
+        ))}
+      </div>
     </main>
   )
 }
